0d55d0aae1de4852712c0665bf965cd011193c77
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2023-06-17 16:29:48 -0300

    feat: Instalação/configuração do Angular Material, implementação dos modulos basicos de layout.
    Foi implementando também o Signal do Angular, para manipular o valor referente ao Toggle do Sidenav.
    
    Author: Phelipe Matos (PHMATOS07)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 @Component({
   selector: 'lab-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SidenavComponent,
+    MatSidenavModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lab-angular-ngx';
+  toggleSidenav = false;
 }
