be92bcf7500453711d7a0707381016f822de8b8c
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2023-06-21 21:36:17 -0300

    feat: Criação e definição estrutura de Rotas para a aplicação.
    Author: Phelipe Matos (PHMATOS07)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(ROUTES),
     provideAnimations()
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const ROUTES: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    loadComponent: () => import('./pages/home/home.component').then(mod => mod.HomeComponent)
+  },
+  {
+    path: 'teste',
+    title: 'Teste',
+    loadComponent: () => import('./pages/teste/teste.component').then(mod => mod.TesteComponent)
+  },
+];
