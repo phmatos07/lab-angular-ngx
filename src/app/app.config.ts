aa3ffe2cc418cf6828fb4f6e2d24164c6dadac2f
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2023-07-04 12:15:28 -0300

    feat: Implementação da estrutura basica do PWA (Service Woykers).
    Author: Phelipe Matos (PHMATOS07)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(ROUTES),
-    provideAnimations()
-  ]
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
