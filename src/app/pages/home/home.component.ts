dac6d0c4a7c63c12ffb50a7539faa9ec45d5f7b5
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2025-01-20 12:13:48 -0300

    feat: Config inicial de um import de web component provido pelo StencilJs.
    Author: Phelipe Matos (PHMATOS07)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lab-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent implements OnInit {
+  
+  readonly POINTS = [
+    {
+      name: '2018',
+      active: true
+    },
+    {
+      name: '2019',
+      active: true
+    },
+    {
+      name: '2020',
+      active: false
+    },
+    {
+      name: '2021',
+      active: false
+    },
+    {
+      name: '2022',
+      active: false
+    },
+    {
+      name: '2023',
+      active: false
+    }
+  ];
 
   constructor() { }
 
